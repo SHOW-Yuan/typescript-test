62ac95c0abf2a244251be97fa681cb542c81b462
yuankl <ykl026708@163.com>
2022-04-03 15:47:48 +0800

    'chore:interface接口练习'

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ let arr3: Array<number | string> = [1, 2, '3'];
 let tuple: [number, string] = [1, '2'];
 // 函数
 let fun = (x: number, y:number) => x + y;
+let funn = (x: number, y: string): string => x + y;
 // 对象
 let obj: { name: string, age: number } = {name: 'zs', age: 18};
 obj.name = 'ls';
@@ -30,6 +31,7 @@ let str2: string | undefined | null = null;
 let noReturn = () => {}
 // any 可以是任意类型的值
 let str3: any = 123;
+str3 = 'qwe';
 // never 永远不会有返回值的函数（也可以说是函数执行不到结尾
 let fun2 = () => {
     throw new Error("报错");
@@ -42,10 +44,12 @@ let fun3 = () => {
 /* enum枚举 */
 // 数字枚举
 enum Role {
-    Rose,
+    Rose = 1,
     Jack,
     Guest
 }
+console.log(Role);
+
 // 字符串枚举
 enum Massage {
     Success = '请求成功...',
@@ -84,3 +88,83 @@ let e3: E.a = 1;
 
 let g1: G = G.b;
 let g2: G.a = G.a;
+
+/* interface 接口 */
+interface List {
+    readonly id: number, // 只读属性 不可更改
+    title: string,
+    age?: number // 这个属性可有可无
+
+    // [x: string]: any // 字符串索引方法 解决不知道有多少参数的问题
+}
+
+interface Result {
+    data: List[]
+}
+
+function render(result: Result){
+    result.data.forEach((val)=>{
+        console.log(val.id + ': ' + val.title);
+        if(val.age){
+            console.log(val.age);
+        }
+
+        // val.id++
+    })
+}
+
+let result = {
+    data: [
+        { id: 1, title: '接口学习', name: 'zs' },
+        { id: 2, title: '接口练习' },
+    ]
+}
+render(result);
+// render({
+//     data: [
+//         { id: 1, title: '接口学习', name: 'zs' },
+//         { id: 2, title: '接口练习' },
+//     ]
+// } as Result) // 类型断言的方式
+
+interface StringArray { // 字符串索引
+    // 用任意的数字去索引StringArray都可以得到一个string
+    [index: number]: string
+}
+let arr: StringArray = ['1', '2'];
+
+interface Names {
+    // 用任意的字符串类型去索引Names都可以得到一个string
+    [x: string]: string
+}
+let names: Names = {
+    name: '张三',
+    name1: '李四'
+}
+
+// 函数类型接口
+let add = (a: number, b: number) => a + b;
+interface Add { // 相当于上面这种形式
+    (a: number, b: number): number
+}
+let add1: Add = (a, b) => a + b;
+
+// 类型别名 （更简单的方式
+type Add1 = (a: number, b: number) => number;
+
+// 混合类型接口
+interface Lib {
+    (): void,
+    version: string,
+    doSomething(): void
+}
+let lib: Lib = (() => {}) as Lib; // 类型断言
+lib.version = '1.0';
+lib.doSomething = () => {}
+
+function getLib(){
+    let lib: Lib = (() => {}) as Lib; // 类型断言
+    lib.version = '1.0';
+    lib.doSomething = () => {};
+    return lib;
+}
